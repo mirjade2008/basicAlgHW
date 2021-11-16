89f60370bfcb271e6d5f0674fac64b988e840a14
mirjade2008 <mirjade2008@yahoo.com>
2021-11-15 17:38:59 -0800

    homework

---
+++ index.js
@@ -0,0 +1,29 @@
+function bouncer(arr) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]) newArray.push(arr[i]);
+    }
+    return newArray;
+  }
+
+
+
+
+
+  function chunkArrayInGroups(arr, size) {
+    let temp = [];
+    let result = [];
+  
+    for (let a = 0; a < arr.length; a++) {
+      if (a % size !== size - 1) temp.push(arr[a]);
+      else {
+        temp.push(arr[a]);
+        result.push(temp);
+        temp = [];
+      }
+    }
+  
+    if (temp.length !== 0) result.push(temp);
+    return result;
+  }
+
